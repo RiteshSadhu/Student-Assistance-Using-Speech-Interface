6034af6f72e8d40598c49e6033dbdcc46ff06763
Ritesh <41541216+RiteshSadhu@users.noreply.github.com>
2022-01-03 10:20:04 +0530

    Update __init__.py

--- project/__init__.py
+++ project/__init__.py
@@ -9,7 +9,7 @@ warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 app = Flask(__name__)
 
-app.secret_key = 'y2JgkQeO9OWyWUpIOXBHnDOvU6YYO6qR'
+app.secret_key = 'your app secret here'
 
 app.config['TESTING'] = True
 
@@ -21,11 +21,8 @@ app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=30)
 
 app.config['SQLALCHEMY_RECORD_QUERIES'] = True
 
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://hello:hellohello@192.168.43.109:3306/pythondb'
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:rootroot@rdspython.cmiy6lo9qske.us-east-1.rds.amazonaws.com:3306/pythondb'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost:3306/pythondb'
 
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://codejxxz_FYUSER:IVD2hsaM-3wn@103.195.185.77:3306/codejxxz_FYProject'
 
 
 app.config['SQLALCHEMY_MAX_OVERFLOW'] = 0
